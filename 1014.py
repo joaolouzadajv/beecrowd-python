1b3970047cdfaf9a44a96e6fdee4ea703aa78053
Joao Louzada <joaolouzada.j.v@gmail.com>
2026-05-27 02:32:08 -0300

    Add files via upload

---
+++ 1014.py
@@ -0,0 +1,9 @@
+# Leitura dos valores de entrada
+X = int(input())
+Y = float(input())
+
+# Cálculo do consumo médio
+consumo_medio = X / Y
+
+# Exibição do resultado formatado com 3 casas decimais
+print(f"{consumo_medio:.3f} km/l")
