0093b09d4b3491e7c5929fb57713b940243f3940
Joao Louzada <joaolouzada.j.v@gmail.com>
2026-05-27 02:14:18 -0300

    Add files via upload

---
+++ 1001.py
@@ -0,0 +1,10 @@
+# Lê os dois valores inteiros
+A = int(input())
+B = int(input())
+
+# Soma os valores e atribui a X
+X = A + B
+
+# Imprime o resultado no formato solicitado
+# O f-string garante o espaço antes e depois do igual
+print(f"X = {X}")
