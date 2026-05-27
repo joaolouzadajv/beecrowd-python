6d5de69a2c136021a8007f1591ad6f76e976a22c
Joao Louzada <joaolouzada.j.v@gmail.com>
2026-05-27 02:16:19 -0300

    Add files via upload

---
+++ 1002.py
@@ -0,0 +1,11 @@
+# Define o valor de n (pi)
+n = 3.14159
+
+# Lê o valor do raio (R) como ponto flutuante
+raio = float(input())
+
+# Calcula a área
+area = n * (raio ** 2)
+
+# Exibe o resultado com 4 casas decimais
+print(f"A={area:.4f}")
