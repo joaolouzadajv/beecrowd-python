044bb6c8b8722a8986c1d07ae54b91a43cd5b4e0
Joao Louzada <joaolouzada.j.v@gmail.com>
2026-05-27 02:22:11 -0300

    Add files via upload

---
+++ 1007.py
@@ -0,0 +1,11 @@
+# Leitura dos quatro valores inteiros
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+# Calculo da diferenca
+diferenca = (A * B - C * D)
+
+# Exibicao do resultado
+print(f"DIFERENCA = {diferenca}")
