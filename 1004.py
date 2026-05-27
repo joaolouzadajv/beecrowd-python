3e38ca74b50c34cb57c1aa67c56be3fb72ecd0f5
Joao Louzada <joaolouzada.j.v@gmail.com>
2026-05-27 02:20:00 -0300

    Add files via upload

---
+++ 1004.py
@@ -0,0 +1,9 @@
+# Leitura dos dois valores inteiros
+valor1 = int(input())
+valor2 = int(input())
+
+# Cálculo do produto
+PROD = valor1 * valor2
+
+# Impressão do resultado no formato solicitado
+print(f"PROD = {PROD}")
