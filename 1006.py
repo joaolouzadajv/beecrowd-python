3488baaa69f4fedde158a6b6cc7750e45b80d7c6
Joao Louzada <joaolouzada.j.v@gmail.com>
2026-05-27 02:22:00 -0300

    Add files via upload

---
+++ 1006.py
@@ -0,0 +1,10 @@
+# Leitura das notas
+A = float(input())
+B = float(input())
+C = float(input())
+
+# Cálculo da média ponderada
+media = (A * 2 + B * 3 + C * 5) / 10
+
+# Exibição do resultado com uma casa decimal
+print(f"MEDIA = {media:.1f}")
